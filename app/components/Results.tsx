d2a232cc385cf12429a44e954c76805fb04faa92
Darel <darelbavcar1@gmail.com>
2023-02-28 21:29:37 +0100

    added card component

---
+++ app/components/Results.tsx
@@ -0,0 +1,43 @@
+import Card from "./Card";
+
+interface Movie {
+  adult: boolean;
+  backdrop_path: string ;
+  belongs_to_collection: any;
+  budget: number;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  homepage: string | null;
+  id: number;
+  imdb_id: string | null;
+  original_language: string;
+  original_title: string;
+  overview: string | null;
+  popularity: number;
+  poster_path: string ;
+
+
+release_date: string;
+status: string;
+title: string;
+vote_average: number;
+vote_count: number;
+}
+
+interface ResultsProps {
+  results: Movie[];
+}
+
+const Results = ({ results }: ResultsProps) => {
+  return (
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
+      {results.map((result) => (
+        <Card result  = {result}/>
+      ))}
+    </div>
+  );
+};
+
+export default Results;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,37 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import Results from './components/Results'
+
+//THIS IS SERVER COMP
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const API_KEY = process.env.API_KEY //get key from .env.local
+
+
+
+export default async function Home({ searchParams }: any) {
+  const genre = searchParams.genre || "fetchTrending"
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 60 } }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+  }
+
+  const data = await res.json();
+
+
+  const results = data.results;
+
+  console.log(results);
+
   return (
-    <main className=''>
- 
-    </main>
+    <div className='flex '>
+      <Results results = {results} />
+    </div>
   )
 }
