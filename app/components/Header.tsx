0d03be2ed46058022041f5fe33046e902b005dcb
Darel <darelbavcar1@gmail.com>
2023-02-28 17:46:19 +0100

    initial commit/working on header comp

---
+++ app/components/Header.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+import MenuItem from "./MenuItem";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import Link from "next/link";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <div className=" flex justify-between 
+     mx-2 max-w-6xl sm:mx-auto items-center py-6">
+      <div className="flex ">
+        <MenuItem title="HOME" address="/" Icon={HomeIcon} />
+        <MenuItem title="ABOUT" address="/about" Icon={InfoIcon} />
+      </div>
+      <div className="">
+        <Link href="/">
+          <h2 className="text-2xl">
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-9 sm:mr-1">
+              IMDb
+            </span>
+            <span className="text-xl hidden sm:inline">Clone</span>
+          </h2>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ app/components/MenuItem.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+
+type Props = {
+    title: string
+    address: string
+    Icon?: any
+}
+
+const MenuItem = ({title, address, Icon}: Props) => {
+  return (
+    <div className=''>
+       
+       <Link href={address} className="mx-4 lg:mx-6 hover:text-amber-500">
+        <Icon className="sm:hidden mx-4 text-4xl text-stone-700 " sx = {{fontSize: "25px"}}/>
+       <p className=' hidden sm:inline my-2 text-sm'>{title}</p>
+       </Link>
+    </div>
+  )
+}
+
+export default MenuItem
