310095900b6d48fd1ad039892ce37ef50400af5a
Darel <darelbavcar1@gmail.com>
2023-03-01 00:43:36 +0100

    fixing bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+
 import Results from './components/Results'
 
 
 
-const inter = Inter({ subsets: ['latin'] })
+export const dynamic = "force-dynamic";
 
-const API_KEY = process.env.API_KEY //get key from .env.local
+const API_KEY = process.env.API_KEY 
 
 
 
@@ -19,7 +17,7 @@ export default async function Home({ searchParams }: any) {
     { next: { revalidate: 60 } }
   );
   if (!res.ok) {
-    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+    throw new Error("Failed to fetch data");
   }
 
   const data = await res.json();
